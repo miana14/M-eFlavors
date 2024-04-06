97d0ff81d919d78cf42cdf232b4fc779aa3cb214
MarianaLD14 <marianadias14b@hotmail.com>
2024-04-06 22:52:36 +0200

    connexion en distant fait

--- modele/bdd.php
+++ modele/bdd.php
@@ -1,16 +1,26 @@
 <?php
 function connexionPDO() {
+    // connexion en local 
+
     $login = "root";
     $mdp = "";
     $bd = "maeflavors";
     $serveur = "localhost";
+
+    // connexion en distant 
+
+    // $login = "gretaxao_loureiroma";
+    // $mdp = "LoureiroMa2023!";
+    // $bd = "gretaxao_loureiroma";
+    // $serveur = "www.greta-bretagne-sud.fr";
+   
 
     try {
         $conn = new PDO("mysql:host=$serveur;dbname=$bd", $login, $mdp, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'')); 
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
     } catch (PDOException $e) {
-        die("Erreur de connexion PDO");
+        die("Erreur de connexion PDO :". $e->getMessage());
     }
 }
 
