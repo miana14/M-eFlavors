045d23668645a6c1a6b0456f8e6b0d23c885c143
MarianaLD14 <marianadias14b@hotmail.com>
2024-04-11 16:23:21 +0200

    ajout d'un nouveau ctl

---
+++ controleur/ajoutCommentaireRecette.php
@@ -0,0 +1,40 @@
+<?php
+
+if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
+    // Un MVC utilise uniquement ses requêtes depuis le contrôleur principal : index.php
+    die('Erreur : ' . basename(__FILE__));
+}
+
+?>
+
+<?php
+
+require_once './modele/mdl_recettes.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $contenu = $_POST['contenu'];
+    $id_recette = $_GET['id_recette'];
+
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+    
+    $adresse_mail_auteur = $_SESSION['email_user'];
+
+    $resultCommentaire = ajoutCommentaire($contenu, $id_recette, $adresse_mail_auteur);
+
+    // var_dump($resultCommentaire);
+
+    if ($resultCommentaire == "Commentaire ajouté avec succès !") {
+        header("Location: ./?action=recette&id_recette=" .$id_recette . $resultCommentaire);
+    }
+
+} else {
+    $_SESSION['msg'] = $resultCommentaire;
+}
+
+
+
+?>
+
+<?php include "vue/vueRecette.php";?>
